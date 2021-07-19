bf54aeb97744ef2b250f7993c3dea3f85d764615
TBL Robert <TBLROBERT99@GMAIL.COM>
2021-07-19 12:22:05 +0600

    Jetstream is Ok now

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
+})->name('dashboard');
